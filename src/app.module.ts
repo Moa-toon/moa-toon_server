b8025d0a29542e0f1dcc3e22e29014fc615cde4a
devRonPark <pbc9236@gmail.com>
2023-01-07 17:22:59 +0900

    [feat] 환경변수 기반 TypeORM MySQL 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `envs/.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts, .js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
