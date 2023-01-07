945a80a48702fab9a12779adc8c7b44ec084a87d
devRonPark <pbc9236@gmail.com>
2023-01-07 16:39:01 +0900

    [feat] Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 
   if (module.hot) {
