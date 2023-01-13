8345051b81a96a64e260b129f4ebe1bc5469adca
devRonPark <pbc9236@gmail.com>
2023-01-13 17:30:25 +0900

    [feat] DB 및 TypeORM Entity 설정 로직 함수로 추출 및 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContentsModule } from './contents/contents.module';
+import { getTypeOrmModule } from './common/utils/getTypeOrmModule';
+import { ContentsModule } from './modules/contents/contents.module';
 
 @Module({
   imports: [
@@ -11,16 +12,7 @@ import { ContentsModule } from './contents/contents.module';
       isGlobal: true,
       envFilePath: `envs/.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: 3306,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts, .js}'],
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
-    }),
+    getTypeOrmModule(),
     ContentsModule,
   ],
   controllers: [AppController],
---
+++ src/common/utils/getTypeOrmModule.ts
@@ -0,0 +1,38 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Author } from 'src/modules/contents/entities/Author';
+import { Content } from 'src/modules/contents/entities/Content';
+import { ContentAuthor } from 'src/modules/contents/entities/ContentAuthor';
+import { ContentGenre } from 'src/modules/contents/entities/ContentGenre';
+import { ContentUpdateDay } from 'src/modules/contents/entities/ContentUpdateDay';
+import { Genre } from 'src/modules/contents/entities/Genre';
+import { Platform } from 'src/modules/contents/entities/Platform';
+import { UpdateDay } from 'src/modules/contents/entities/UpdateDay';
+
+// app의 DB 설정 및 TypeOrm Entity 설정에 대한 값 설정을 한다.
+const isProd = process.env.NODE_ENV === 'production' ? true : false;
+export function getTypeOrmModule() {
+  return TypeOrmModule.forRootAsync({
+    useFactory: () => {
+      return {
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [
+          Content,
+          Platform,
+          Author,
+          Genre,
+          UpdateDay,
+          ContentAuthor,
+          ContentGenre,
+          ContentUpdateDay,
+        ],
+        synchronize: !isProd ? true : false,
+        logging: !isProd ? true : false,
+      };
+    },
+  });
+}
