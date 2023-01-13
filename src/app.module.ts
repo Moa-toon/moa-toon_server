313fad5b65b47894edd9d4591833eb3812d14852
devRonPark <pbc9236@gmail.com>
2023-01-13 17:36:29 +0900

    [feat] Joi 라이브러리를 적용해 환경변수 유효성 검증

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 import { getTypeOrmModule } from './common/utils/getTypeOrmModule';
 import { ContentsModule } from './modules/contents/contents.module';
 
@@ -11,6 +12,13 @@ import { ContentsModule } from './modules/contents/contents.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `envs/.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
     }),
     getTypeOrmModule(),
     ContentsModule,
