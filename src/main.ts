5286b24ce3d65fb21cac78f9651aff1bec387a60
devRonPark <pbc9236@gmail.com>
2023-01-13 17:02:47 +0900

    [chore] util 디렉토리를 common 디렉토리 하위로 변경 및 utils 로 디렉토리명 변경

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './util/swagger';
+import { setupSwagger } from './common/utils/swagger';
 declare const module: any;
 
 async function bootstrap() {
