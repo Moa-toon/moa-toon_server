9114412d37441925e3023972e9c8fc19d4fbb3f8
devRonPark <pbc9236@gmail.com>
2023-01-21 22:20:51 +0900

    [refactor] 하나의 웹툰 정보 추출 로직 getWebtoonItemInfo 함수로 추출 및 Webtoon 타입 선언, 적용

--- src/scrape-content/scrape-content.service.ts
+++ src/scrape-content/scrape-content.service.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { load } from 'cheerio';
 
+type Webtoon = {
+  id: string;
+  title: string;
+  authors: Array<string>;
+  url: string;
+  thumbnailPath: string;
+  platform: 'naver' | 'kakao' | 'kakaopage';
+  updateDays: Array<string>;
+};
+
 @Injectable()
 export class ScrapeContentService {
   private readonly NAVER_WEBTOON_URL = 'https://m.comic.naver.com/webtoon';
@@ -22,17 +32,7 @@ export class ScrapeContentService {
     return dailyWebtoons;
   }
 
-  async scrapeNaverDailyWebtoons(url: string): Promise<
-    Array<{
-      id: string;
-      title: string;
-      authors: Array<string>;
-      url: string;
-      thumbnailPath: string;
-      platform: 'naver' | 'kakao' | 'kakaopage';
-      updateDays: Array<string>;
-    }>
-  > {
+  async scrapeNaverDailyWebtoons(url: string): Promise<Array<Webtoon>> {
     console.log('네이버 데일리 웹툰 콘텐츠 스크래핑 작업!!!');
     console.log(`타겟 url: ${url}`);
     // url에 대해 axios.get 요청
@@ -42,41 +42,12 @@ export class ScrapeContentService {
     const webtoonItemList = $(
       '#ct > .section_list_toon > ul.list_toon > li.item > a',
     );
-    const webtoons: Array<{
-      id: string;
-      title: string;
-      authors: Array<string>;
-      url: string;
-      thumbnailPath: string;
-      platform: 'naver' | 'kakao' | 'kakaopage';
-      updateDays: Array<string>;
-    }> = [];
+    const webtoons: Array<Webtoon> = [];
     console.log(`콘텐츠 개수: ${webtoonItemList.length}`);
 
     for (const webtoonItem of webtoonItemList) {
-      const webtoonElem = $(webtoonItem);
-      const contentUrl = webtoonElem.attr('href');
-      const contentId = contentUrl.split('?titleId=')[1].split('&')[0];
-      const title = webtoonElem
-        .find('div.info > div > strong > span')
-        .text()
-        .trim();
-      const thumbnailPath = webtoonElem.find('.thumbnail img').attr('src');
-      const authors = webtoonElem
-        .find('div.info > span.author')
-        .text()
-        .replace(/\n/g, '')
-        .replace(/\t/g, '')
-        .split(' / ');
-      webtoons.push({
-        id: contentId,
-        title,
-        authors,
-        url: `https://m.comic.naver.com${contentUrl}`,
-        thumbnailPath,
-        platform: 'naver',
-        updateDays: ['daily'],
-      });
+      const webtoon = this.getWebtoonItemInfo($, webtoonItem);
+      webtoons.push(webtoon);
     }
 
     return webtoons;
@@ -94,5 +65,31 @@ export class ScrapeContentService {
 
   loadHtml(html: string): cheerio.Root {
     return load(html);
+  }
+
+  getWebtoonItemInfo($: cheerio.Root, element: cheerio.Element): Webtoon {
+    const webtoonElem = $(element);
+    const contentUrl = webtoonElem.attr('href');
+    const contentId = contentUrl.split('?titleId=')[1].split('&')[0];
+    const title = webtoonElem
+      .find('div.info > div > strong > span')
+      .text()
+      .trim();
+    const thumbnailPath = webtoonElem.find('.thumbnail img').attr('src');
+    const authors = webtoonElem
+      .find('div.info > span.author')
+      .text()
+      .replace(/\n/g, '')
+      .replace(/\t/g, '')
+      .split(' / ');
+    return {
+      id: contentId,
+      title,
+      authors,
+      url: `https://m.comic.naver.com${contentUrl}`,
+      thumbnailPath,
+      platform: 'naver',
+      updateDays: ['daily'],
+    };
   }
 }
