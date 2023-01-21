07c889dccea2fd74166ebdb7ef9c41660542d702
devRonPark <pbc9236@gmail.com>
2023-01-21 21:36:39 +0900

    [feat] axios 설치 및 네이버 데일리 웹툰 콘텐츠 스크래핑 axios.get 요청

--- src/scrape-content/scrape-content.service.ts
+++ src/scrape-content/scrape-content.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class ScrapeContentService {
+  private readonly NAVER_WEBTOON_URL = 'https://m.comic.naver.com/webtoon';
   async getContentsByPlatform(platform: string) {
-    console.log(platform);
+    if (platform === 'naver') {
+      console.log('네이버 웹툰');
+      return this.getNaverWebtoons(this.NAVER_WEBTOON_URL);
+    }
+
     return platform;
+  }
+
+  async getNaverWebtoons(baseUrl: string) {
+    // Daily 웹툰 콘텐츠 스크래핑
+    const dailyWebtoons = await this.scrapeNaverDailyWebtoons(
+      `${baseUrl}/weekday?week=dailyPlus`,
+    );
+    return dailyWebtoons;
+  }
+
+  async scrapeNaverDailyWebtoons(url: string) {
+    console.log('네이버 데일리 웹툰 콘텐츠 스크래핑 작업!!!');
+    console.log(`타겟 url: ${url}`);
+    // url에 대해 axios.get 요청
+    const html: { data: string } = await axios.get(url);
+    console.log(html);
+    return html.data;
   }
 }
