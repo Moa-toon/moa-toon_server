08549dc09bca5305c43657ddd6cae7e124f7e6de
devRonPark <pbc9236@gmail.com>
2023-01-21 21:39:29 +0900

    [refactor] axios.get 로직 getHtmlData 함수로 추출

--- src/scrape-content/scrape-content.service.ts
+++ src/scrape-content/scrape-content.service.ts
@@ -25,8 +25,18 @@ export class ScrapeContentService {
     console.log('네이버 데일리 웹툰 콘텐츠 스크래핑 작업!!!');
     console.log(`타겟 url: ${url}`);
     // url에 대해 axios.get 요청
-    const html: { data: string } = await axios.get(url);
-    console.log(html);
-    return html.data;
+    const htmlData = await this.getHtmlData(url);
+
+    return htmlData;
+  }
+
+  async getHtmlData(url: string): Promise<string> {
+    try {
+      const html: { data: string } = await axios.get(url);
+      return html.data;
+    } catch (err) {
+      console.error(err);
+      return err.message;
+    }
   }
 }
