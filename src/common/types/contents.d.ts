eee4b2a09a0922507ab73910fa86deb8db519b61
devRonPark <pbc9236@gmail.com>
2023-01-22 23:02:33 +0900

    [feat] contents 관련 타입 파일 생성 및 이동

---
+++ src/common/types/contents.d.ts
@@ -0,0 +1,49 @@
+export type WebtoonSimpleInfo = {
+  id: string;
+  title: string;
+  authors: Array<string>;
+  url: string;
+  thumbnailPath: string;
+  platform: 'naver' | 'kakao' | 'kakaopage';
+  updateDays: Array<string>;
+  additional: {
+    isNew: boolean;
+    isAdult: boolean;
+    isPaused: boolean;
+    isUpdated: boolean;
+  };
+};
+
+export type WebtoonEpisodeInfo = {
+  name: string;
+  url: string;
+  thumbnailPath: string;
+  createDate: string;
+  isFree?: boolean;
+};
+
+export type WebtoonAdditionalInfo = {
+  url: string;
+  summary: string;
+  description: string;
+  genres: Array<string>;
+  episodes: Array<WebtoonEpisodeInfo>;
+};
+
+export type Webtoon = {
+  id: string;
+  title: string;
+  authors: Array<string>;
+  url: string;
+  thumbnailPath: string;
+  platform: 'naver' | 'kakao' | 'kakaopage';
+  updateDays: Array<string>;
+  additional: {
+    isNew: boolean;
+    isAdult: boolean;
+    isPaused: boolean;
+    isUpdated: boolean;
+  };
+  summary: string;
+  description: string;
+};
