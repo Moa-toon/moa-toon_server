5355d339775bc7851e7bab46424b0365ee38f851
devRonPark <pbc9236@gmail.com>
2023-01-22 12:41:40 +0900

    [feat] 웹툰 데이터 수집 컨트롤러 메서드 updateDay 쿼리 추가

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,11 +1,25 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { writeFileSync } from 'fs';
 import { ScrapeContentService } from 'src/scrape-content/scrape-content.service';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly scrapeContentService: ScrapeContentService) {}
   @Get('/contents')
-  async getContentsByPlatform(@Query('platform') platform: string) {
-    return this.scrapeContentService.getContentsByPlatform(platform);
+  async getContentsByPlatform(
+    @Query('platform') platform: string,
+    @Query('updateDay') updateDay: string,
+  ) {
+    try {
+      const contents = await this.scrapeContentService.getContentsByPlatform(
+        platform,
+        updateDay,
+      );
+      writeFileSync('naver.webtoon.json', JSON.stringify(contents));
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 }
