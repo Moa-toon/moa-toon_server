e8813e28cdd9250c0748a8abd6411e82ca6ff6eb
devRonPark <pbc9236@gmail.com>
2023-01-25 20:28:44 +0900

    [feat] class-validator 설치 및 GetContentsQueryDto 에 validation 적용

--- src/modules/contents/dto/request.ts
+++ src/modules/contents/dto/request.ts
@@ -5,7 +5,7 @@ import {
   UpdateDayCode,
 } from 'src/common/types/contents';
 import { PaginationOptions } from 'src/common/utils/pagination/pagination.options';
-import { Platform } from '../entities/Platform';
+import { IsNumberString, IsNotEmpty, IsEnum } from 'class-validator';
 
 export class GetContentsReqQueryDto implements PaginationOptions {
   @ApiProperty({
@@ -14,33 +14,47 @@ export class GetContentsReqQueryDto implements PaginationOptions {
     examples: ContentType,
     required: true,
   })
+  @IsEnum(ContentType)
+  @IsNotEmpty()
   readonly type: ContentType;
+
   @ApiProperty({
     type: String,
     description: '콘텐츠 제공 플랫폼',
-    examples: Platform,
+    examples: PlatformType,
     required: true,
   })
+  @IsEnum(PlatformType)
+  @IsNotEmpty()
   readonly platform: PlatformType;
+
   @ApiProperty({
     type: String,
     description: '콘텐츠 업데이트 요일',
     examples: UpdateDayCode,
     required: true,
   })
+  @IsEnum(UpdateDayCode)
+  @IsNotEmpty()
   readonly updateDay: UpdateDayCode;
+
   @ApiProperty({
     type: Number,
     description: '페이지',
     example: 1,
     required: true,
   })
+  @IsNumberString()
+  @IsNotEmpty()
   readonly page: number;
+
   @ApiProperty({
     type: Number,
     description: '불러올갯수',
     example: 10,
     required: true,
   })
+  @IsNumberString()
+  @IsNotEmpty()
   readonly take: number;
 }
