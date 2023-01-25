f09349f6d27abf81c6b4235527f01d73a6c042d5
devRonPark <pbc9236@gmail.com>
2023-01-25 20:05:38 +0900

    [feat] Content 목록 조회 API Swagger 명세 추가

--- src/modules/contents/contents.controller.ts
+++ src/modules/contents/contents.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ContentsService } from './contents.service';
 import { GetContentsReqQueryDto } from './dto/request';
+import { ContentsResponse } from './dto/response';
 
+@ApiTags('콘텐츠 API')
 @Controller('contents')
 export class ContentsController {
   constructor(private readonly contentsService: ContentsService) {}
 
-  @Get('/')
+  @Get('')
+  @ApiOkResponse({
+    description: '성공',
+    type: ContentsResponse,
+  })
+  @ApiOperation({ summary: '콘텐츠 목록 조회 API', description: '' })
   async getContents(@Query() query: GetContentsReqQueryDto) {
     const contents = await this.contentsService.getContents(query);
     return contents;
